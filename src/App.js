cd4329d24fe70c2cd2d0cb285c790b4a3742ebb3
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-12 16:13:52 +0500

    feat: create About, Home, Contacts components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Contacts from './components/Contacts';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+     <BrowserRouter>
+      <Link className="link" to='/'>Home</Link>
+      <Link className="link" to='/about'>About</Link>
+      <Link className="link" to='/contacts'>Contacts</Link>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
+     </BrowserRouter> 
     </div>
   );
 }
